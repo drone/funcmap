600b661493d8685e5513aa3a9ade8d6101f9efcf
Brad Rydzewski <bradley.rydzewski@harness.io>
2024-02-27 10:15:50 -0500

    added tests for escape funcs

--- escape_test.go
+++ escape_test.go
@@ -3,3 +3,49 @@
 // license that can be found in the LICENSE file.
 
 package funcmap
+
+import "testing"
+
+func TestHTMLEscape(t *testing.T) {
+	tests := []struct {
+		data map[string]interface{}
+		text string
+		want string
+	}{
+		{
+			data: map[string]interface{}{"input": "<b>hello world</b>"},
+			text: `{{ htmlEscape .input }}`,
+			want: `&lt;b&gt;hello world&lt;/b&gt;`,
+		},
+	}
+	for _, test := range tests {
+		out, err := execute(test.text, test.data)
+		if err != nil {
+			t.Error(err)
+		} else if out != test.want {
+			t.Errorf("Want template result %s, got %s", test.want, out)
+		}
+	}
+}
+
+func TestHTMLUnescape(t *testing.T) {
+	tests := []struct {
+		data map[string]interface{}
+		text string
+		want string
+	}{
+		{
+			data: map[string]interface{}{"input": "&lt;b&gt;hello world&lt;/b&gt;"},
+			text: `{{ htmlUnescape .input }}`,
+			want: `<b>hello world</b>`,
+		},
+	}
+	for _, test := range tests {
+		out, err := execute(test.text, test.data)
+		if err != nil {
+			t.Error(err)
+		} else if out != test.want {
+			t.Errorf("Want template result %s, got %s", test.want, out)
+		}
+	}
+}
